b2567938d622c8a4525112e7a0016b725386e98e
YGJH <jihuty@gmail.com>
2024-06-02 00:57:51 +0800

    Heap become bigger

--- Sort.h
+++ Sort.h
@@ -6,7 +6,7 @@ class Sort {
         static constexpr bool(*cmp_)(T &a , T &b) = [](T &a , T &b)->bool { return a < b; };
     public:
         Sort(T *st , T *en , bool(*cmp)(T &a , T &b) = cmp_ ) {
-            if(en - st < 32) {
+            if(en - st < 100) {
                 HeapSort<T> he(st);
                 he.sort(st , en , cmp );
             }
